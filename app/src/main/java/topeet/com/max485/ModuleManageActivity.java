d6f26c726dd61633632c945e848bc1c850645d48
jaylee <mr_lizhengjie@163.com>
2018-07-08 08:15:31 +0800

    First commit

---
+++ app/src/main/java/topeet/com/max485/ModuleManageActivity.java
@@ -0,0 +1,13 @@
+package topeet.com.max485;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ModuleManageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_module_manage);
+    }
+}
